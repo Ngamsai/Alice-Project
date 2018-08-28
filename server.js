67339b8e722054a809eb6f91529aa140bab5fe5c
nanearnano <nanearnano@gmail.com>
2018-08-28 21:04:07 +0700

    first commit

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+var bodyParser = require('body-parser');
 var express = require('express');
 var app = express();
 var path = require('path');
@@ -8,7 +9,14 @@ var port = process.env.PORT || 5000;
 //     res.sendFile(__dirname + '/index.html');
 // });
 app.use('/', express.static(path.join(__dirname, 'public')))
-
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.post('/', (req, res) => {
+    
+    res.send('Hello World')
+    console.log(req.body.queryResult.parameters);
+    res.send('8585')
+})
 http.listen(port, function () {
     console.log('listening on *: ' + port);
 });
