ed73ed232759821f3147dda05a52774c46e490bd
nanearnano <nanearnano@gmail.com>
2018-10-29 19:32:00 +0700

    detect cashing complete

--- server.js
+++ server.js
@@ -7,10 +7,25 @@ var io = require('socket.io')(http); //server
 var port = process.env.PORT || 3000;
 
 var text = null;
-var position = [[-30,-143]];
-var x = -30,y = -143;
+var maze = [[0,1,0,1,0,1,0,1,0,1,0],
+            [1,0,0,0,1,0,0,0,0,0,1],
+            [0,0,0,0,0,1,0,0,0,0,0],
+            [1,0,0,0,1,0,0,0,1,0,1],
+            [0,0,0,1,0,0,0,1,0,1,0],
+            [1,0,0,0,0,0,0,0,0,0,1],
+            [0,1,0,1,0,0,0,0,0,0,0],
+            [1,0,0,0,0,0,1,0,1,0,1],
+            [0,0,0,0,0,0,0,0,0,0,0],
+            [1,0,0,0,1,0,0,0,0,0,1],
+            [0,1,0,1,0,0,0,0,0,0,0],
+            [1,0,0,0,0,0,0,0,0,0,1],
+            [0,1,0,1,0,1,0,1,0,1,0]];
+var position = [[11,1]];
+var maze_x = 11;
+var maze_y = 1;
 var direction = 'E';
 var state = 'maze1';
+var position_flag = true;
 var order,distance,startgame,turn,name,character,replay,ansQ2,forward_backward_direction,left_right_direction,anser,language;
 
 app.use('/', express.static(path.join(__dirname, 'public')))
@@ -23,7 +38,7 @@ app.post('/exam', (req, res) => {
     if (!req.body) return res.sendStatus(400)
     var keep = req.body.queryResult.parameters;
     var responsetext = req.body.queryResult.fulfillmentText;
-    console.log('res is ',responsetext);
+//     console.log('res is ',responsetext);
     
  
     if(text != null){
@@ -64,333 +79,279 @@ app.post('/exam', (req, res) => {
       console.log('ansQ2 is ',anser);
     }
   
-  
+  //when maze state will calculate this function
     if (order != null && distance != null){
       if (language == 'th'){
         if (order == "ตรงไป"){
           order = "forward";
-          for (var a=0; a<distance; a++){
-            if(direction == 'N'){
-              y += 57;
-            }
-            else if(direction == 'S'){
-              y -= 57;
-            }
-            else if(direction == 'W'){
-              x -= 57;
-            }
-            else if(direction == 'E'){
-              x += 57;
-            }
-            for (var i = 0; i<position.length; i++){
-              if(position[i][0] == x){
-                if (position[i][1] == y){
-                  responsetext = 'ไม่สามารถเดินซ้ำเส้นทางเดิมได้ กลับไปเริ่มต้นอีกครั้ง';
-                } 
-              }
-            }
-            position.push([x,y]);
-            if (responsetext == 'ไม่สามารถเดินซ้ำเส้นทางเดิมได้ กลับไปเริ่มต้นอีกครั้ง'){
-                position.splice(1, position.length);
-                x = -30;
-                y = -143;
-                direction = 'E';
-            }
-          }
         }
         else if (order == "ถอยหลัง"){
           order = "backward";
-          for (var b=0; b<distance; b++){
-            if(direction == 'N'){
-              y-=57;
-            }
-            else if(direction == 'S'){
-              y+=57;
-            }
-            else if(direction == 'W'){
-              x += 57;
-            }
-            else if(direction == 'E'){
-              x -= 57;
-            }
-            for (var j = 0; j<position.length; j++){
-              if(position[j][0] == x){
-                if (position[j][1] == y){
-                  responsetext = 'ไม่สามารถเดินซ้ำเส้นทางเดิมได้ กลับไปเริ่มต้นอีกครั้ง';
-                } 
-              }
-            }
-            position.push([x,y]);
-            if (responsetext == 'ไม่สามารถเดินซ้ำเส้นทางเดิมได้ กลับไปเริ่มต้นอีกครั้ง'){
-                position.splice(1, position.length);
-                x = -30;
-                y = -143;
-                direction = 'E';
-            }
-          }
         }
         else if (order == "เลี้ยวซ้าย"){
           order = "left";
-          for (var c=0; c<distance; c++){
-            if (direction == 'E'){
-              direction = 'N';
-            }
-            else if (direction == 'N'){
-              direction = 'W';
-            }
-            else if (direction == 'W'){
-              direction = 'S';
-            }
-            else if (direction == 'S'){
-              direction = 'E';
-            }
-          }
         }
         else if (order == "เลี้ยวขวา"){
           order = "right";
-          for (var d=0; d<distance; d++){
-            if (direction == 'E'){
-              direction = 'S';
-            }
-            else if (direction == 'S'){
-              direction = 'W';
-            }
-            else if (direction == 'W'){
-              direction = 'N';
-            }
-            else if (direction == 'N'){
-              direction = 'E';
-            }
-          }
         }
-        console.log(position);
+        ComputePosition();
+        if (responsetext == 'start new') {
+          responsetext = 'ไม่สามารถเดินซ้ำเส้นทางเดิมได้ กลับไปเริ่มต้นอีกครั้ง';
+        }else if (responsetext == 'crashing'){
+          responsetext = 'ไม่สามารถไปเส้นทางนี้ได้'
+        }
+        checkState();
+        if ( responsetext == 'go to maze 2'){
+          responsetext = 'ไปยังด่านเขาวงกตที่ 2';
+        }
+        else if (responsetext == 'go to maze 3'){
+          responsetext = 'ไปยังด่านเขาวงกตที่ 3';
+        }
+        else if (responsetext == 'go to maze 4'){
+          responsetext = 'ไปยังด่านเขาวงกตที่ 4';
+        }
+        else if (responsetext == 'go to maze 5'){
+           responsetext = 'ไปยังด่านเขาวงกตที่ 5';
+        }
+        else if (responsetext == 'I keep key already'){
+           responsetext = 'เก็บกุญแจได้แล้ว'; 
+        }
+        else if (responsetext == 'you have to keep a key frist'){
+           responsetext = 'ต้องไปเก็บกุญแจก่อนมาไขประตูนะ';
+        }
+        else if (responsetext == 'go to next state'){
+           responsetext = 'ไปยัด่านต่อไปได้เลย';
+         }
       }
       else if (language == 'en'){
-        if (order == "forward"){
-          order = "forward";
-          for (var e=0; e<distance; e++){
-            if(direction == 'N'){
-              y += 57;
-            }
-            else if(direction == 'S'){
-              y -= 57;
-            }
-            else if(direction == 'W'){
-              x -= 57;
-            }
-            else if(direction == 'E'){
-              x += 57;
-            }
-            for (var k = 0; k<position.length; k++){
-              if(position[k][0] == x){
-                if (position[k][1] == y){
-                  responsetext = 'start new';
-                  position.splice(1, position.length);
-                  x = -30;
-                  y = -143;
-                } 
-              }
-            }
-            position.push([x,y]);
-          }
-        }
-        else if (order == "backward"){
-          order = "backward";
-          for (var f=0; f<distance; f++){
-            if(direction == 'N'){
-              y-=57;
-            }
-            else if(direction == 'S'){
-              y+=57;
-            }
-            else if(direction == 'W'){
-              x += 57;
-            }
-            else if(direction == 'E'){
-              x -= 57;
-            }
-            for (var l = 0; l<position.length; l++){
-              if(position[l][0] == x){
-                if (position[l][1] == y){
-                  responsetext = 'start new';
-                  position.splice(1, position.length);
-                  x = -30;
-                  y = -143;
-                } 
-              }
-            }
-            position.push([x,y]);
-          }
-        }
-        else if (order == "left"){
-          order = "left";
-          for (var g=0; g<distance; g++){
-            if (direction == 'E'){
-              direction = 'N';
-            }
-            else if (direction == 'N'){
-              direction = 'W';
-            }
-            else if (direction == 'W'){
-              direction = 'S';
-            }
-            else if (direction == 'S'){
-              direction = 'E';
-            }
-          }
-        }
-        else if (order == "right"){
-          order = "right";
-          for (var q=0; q<distance; q++){
-            if (direction == 'E'){
-              direction = 'S';
-            }
-            else if (direction == 'S'){
-              direction = 'W';
-            }
-            else if (direction == 'W'){
-              direction = 'N';
-            }
-            else if (direction == 'N'){
-              direction = 'E';
-            }
-          }
-        }
-        console.log(position);
+        ComputePosition();
+        checkState();
       }
     }
   
-    if (language == 'th'){
-      if (state == 'maze1'){
-        if (x == 141 && y == 28){
-          responsetext = 'ไปยังด่านเขาวงกตที่ 2';
-          state = 'maze2';
-          position.splice(1, position.length);
-          x = -30;
-          y = -143;
-          direction = 'E';
+    function ComputePosition (){
+      if (order == "forward"){
+        for (var e=0; e<distance; e++){
+          if(direction == 'N'){
+            if (maze[maze_x-1][maze_y] === 0){
+              maze_x -= 2; 
+              console.log('maze_x ',maze_x);
+              position_flag = true
+            }
+            else {
+              position_flag = false
+            }
+          }
+          else if(direction == 'S'){
+            if (maze[maze_x+1][maze_y] === 0){
+              maze_x += 2; 
+              position_flag = true
+            }
+            else {
+              position_flag = false
+            }
+          }
+          else if(direction == 'W'){
+            if (maze[maze_x][maze_y-1] === 0){
+              maze_y -= 2; 
+              position_flag = true
+            }
+            else {
+              position_flag = false
+            }
+          }
+          else if(direction == 'E'){
+            if (maze[maze_x][maze_y+1] === 0){
+              maze_y += 2; 
+              position_flag = true
+            }
+            else {
+              position_flag = false
+            }
+          }
+          console.log('position length ',position.length);
+          console.log('x ',maze_x);
+          console.log('y',maze_y);
+          console.log('dir ',direction);
+          for (var k = 0; k<position.length; k++){
+            console.log('k',k);
+            if(position[k][0] == maze_x){
+              if (position[k][1] == maze_y){
+                responsetext = 'start new';
+              } 
+            }
+          }
+          if (position_flag){
+            position.push([maze_x,maze_y]);  
+          }else{
+            responsetext = 'crashing';
+          }
+          if (responsetext == 'start new'){
+              resetPosition(position);
+          }
         }
       }
-      else if (state == 'maze2'){
-        if (x == 141 && y == 85){
-          responsetext = 'ไปยังด่านเขาวงกตที่ 3';
-          state = 'maze3';
-          position.splice(1, position.length);
-          x = -30;
-          y = -143;
-          direction = 'E';
-        }
-      }
-      else if (state == 'maze3'){
-        if (x == 27 && y == -86){
-           responsetext = 'ไปยังด่านเขาวงกตที่ 4';
-           state = 'maze4';
-           position.splice(1, position.length);
-           x = -30;
-           y = -143;
-           direction = 'E';
-        }
-      }
-      else if (state == 'maze4'){
-        if (x == 27 && y == 85){
-            responsetext = 'ไปยังด่านเขาวงกตที่ 5';
-            state = 'maze5';
-            position.splice(1, position.length);
-            x = -30;
-            y = -143;
-            direction = 'E';
-        }
-      }
-      else if (state == 'maze6'){
-        if (x == 141 && y == 28){
-            responsetext = 'เก็บกุญแจได้แล้ว'; 
-        }else 
-            responsetext = 'ต้องไปเก็บกุญแจก่อนมาไขประตูนะ';
-
-        if (x == 198 && y == 85){  
-            for (var p = 0; p<position.length; p++){
-              if(position[p][0] == x == 141){
-                 if (position[p][1] == y == 28){
-                   responsetext = 'จบด่านเขาวงกตแล้ว เก่งมากๆเลย';
-                   position.splice(1, position.length);
-                   x = -30;
-                   y = -143;
-                   state = 'Q2';
-                   var randomtrees = Math.floor(Math.random() * 10) + 1;
-                   var randomstone = Math.floor(Math.random() * 10) + 1;
-                   io.emit('Q2',randomtrees,randomstone)
+      else if (order == "backward"){
+        for (var f=0; f<distance; f++){
+          if(direction == 'N'){
+            if (maze[maze_x+1][maze_y] === 0){
+              maze_x += 2; 
+              position_flag = true
+            }
+            else {
+              position_flag = false
+            }
+          }
+          else if(direction == 'S'){
+            if (maze[maze_x-1][maze_y] === 0){
+              maze_x -= 2; 
+              position_flag = true
+            }
+            else {
+              position_flag = false
+            }
+          }
+          else if(direction == 'W'){
+            if (maze[maze_x][maze_y+1] === 0){
+              maze_y += 2; 
+              position_flag = true
+            }
+            else {
+              position_flag = false
+            }
+          }
+          else if(direction == 'E'){
+            if (maze[maze_x][maze_y-1] === 0){
+              maze_y -= 2; 
+              position_flag = true
+            }
+            else {
+              position_flag = false
+            }
+          }
+          for (var l = 0; l<position.length; l++){
+            if(position[l][0] == maze_x){
+              if (position[l][1] == maze_y){
+                responsetext = 'start new';
                  } 
-               }
-             } 
-         }
+            }
+          }
+          if (position_flag){
+            position.push([maze_x,maze_y]);  
+          }else{
+            responsetext = 'crashing';
+          }
+          if (responsetext == 'start new'){
+              resetPosition(position);
+          }
+        }
       }
+      else if (order == "left"){
+        for (var g=0; g<distance; g++){
+          if (direction == 'E'){
+            direction = 'N';
+          }
+          else if (direction == 'N'){
+            direction = 'W';
+          }
+          else if (direction == 'W'){
+            direction = 'S';
+          }
+          else if (direction == 'S'){
+            direction = 'E';
+          }
+        }
+      }
+      else if (order == "right"){
+        for (var q=0; q<distance; q++){
+          if (direction == 'E'){
+            direction = 'S';
+          }
+          else if (direction == 'S'){
+            direction = 'W';
+          }
+          else if (direction == 'W'){
+            direction = 'N';
+          }
+          else if (direction == 'N'){
+            direction = 'E';
+          }
+        }
+      }
+      console.log(direction);
+      console.log(position);
     }
   
-    if (language == 'en'){
+   function resetPosition(){
+     position.splice(1, position.length);
+     maze_x = 11;
+     maze_y = 1;
+     direction = 'E';     
+   }
+  
+    function checkState(){
       if (state == 'maze1'){
-        if (x == 141 && y == 28){
+        if (maze_x == 5 && maze_y == 7){
           responsetext = 'go to maze 2';
           state = 'maze2';
-          position.splice(1, position.length);
-          x = -30;
-          y = -143;
-          direction = 'E';
+          resetPosition();
         }
       }
       else if (state == 'maze2'){
-        if (x == 141 && y == 85){
+        if (maze_x == 3 && maze_y == 7){
           responsetext = 'go to maze 3';
           state = 'maze3';
-          position.splice(1, position.length);
-          x = -30;
-          y = -143;
-          direction = 'E';
+          resetPosition(position);
         }
       }
       else if (state == 'maze3'){
-        if (x == 27 && y == -86){
+        if (maze_x == 9 && maze_y == 3){
            responsetext = 'go to maze 4';
            state = 'maze4';
-           position.splice(1, position.length);
-           x = -30;
-           y = -143;
-           direction = 'E';
+           resetPosition(position);
         }
       }
       else if (state == 'maze4'){
-        if (x == 27 && y == 85){
+        if (maze_x == 3 && maze_y == 3){
             responsetext = 'go to maze 5';
             state = 'maze5';
-            position.splice(1, position.length);
-            x = -30;
-            y = -143;
-            direction = 'E';
+            resetPosition(position);
+        }
+      }
+      else if (state == 'maze5'){
+        if (maze_x == 3 && maze_y == 9){
+            responsetext = 'go to maze 5';
+            state = 'maze5';
+            resetPosition(position);
         }
       }
       else if (state == 'maze6'){
-        if (x == 141 && y == 28){
+        if (maze_x == 5 && maze_y == 7){
             responsetext = 'I keep key already'; 
-        }else 
-            responsetext = 'you have to keep a key frist';
-
-        if (x == 198 && y == 85){  
-            for (var r = 0; r<position.length; r++){
-              if(position[r][0] == x == 141){
-                 if (position[r][1] == y == 28){
-                   responsetext = 'go to next state';
-                   position.splice(1, position.length);
-                   x = -30;
-                   y = -143;
-                   state = 'Q2';
-//                    randomtrees = Math.floor(Math.random() * 10) + 1;
-//                    randomstone = Math.floor(Math.random() * 10) + 1;
-//                    io.emit('Q2',randomtrees,randomstone)
-                 } 
-               }
-             } 
-         }
+        }
+        else if (maze_x == 3 && maze_y == 1){  
+          if (text == 'havekey'){
+             responsetext = 'go to next state';
+             resetPosition(position);
+             state = 'Q2';
+             var randomtrees = Math.floor(Math.random() * 10) + 1;
+             var randomstone = Math.floor(Math.random() * 10) + 1;
+             io.emit('Q2',randomtrees,randomstone)
+           } else {
+             responsetext = 'You have to keep a key first';
+           }
+        }
       }
+      console.log('responsetext from checkState is ',responsetext);
      }
+    
+//     function detectCrash (collision){
+//       responsetext = 'crashing';
+//       console.log('crashing');
+//       position.splice(1, position.length);
+//       maze_x = -30;
+//       maze_y = -143;
+//     }
   
 //     else if (state == 'Q2'){
 //       responsetext = 'there are many trees ?';
@@ -412,8 +373,10 @@ app.post('/exam', (req, res) => {
     //emit to scratchX game and scratchX show log code 
     io.emit('chat',order,distance,startgame,character,replay,ansQ2,state);
     io.emit('symbols',order,distance,state);
- 
-    return res.json(responseObj);
+    setTimeout(function(){
+       console.log('send already');
+       return res.json(responseObj);
+    },2000)
     
  })
 
