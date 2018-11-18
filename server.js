dfb317434250fe56ed789f4c973e6257f609f68c
nanearnano <nanearnano@gmail.com>
2018-11-18 17:06:48 +0700

    detect input character10

--- server.js
+++ server.js
@@ -95,7 +95,7 @@ app.post('/', (req, res) => {
     }
     else if (character != null ) {
       status_state = 2;
-      console.log('actor is ' , type(character));
+      console.log('actor is ' , typeof character);
       if (character != 1 || character != 2 ){
         responsetext = 'เลือกได้เฉพาะตัวที่ 1 หรือ 2 เท่านั้นนะคะ';
       }
