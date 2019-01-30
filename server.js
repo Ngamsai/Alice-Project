c9b17cf3f8d30f070411c3204fce1beb94415238
NARUMON SETDUSIT <narumon_setdusit@cmu.ac.th>
2019-01-30 13:08:39 +0700

    detect input character

--- server.js
+++ server.js
@@ -99,10 +99,10 @@ app.post('/', (req, res) => {
     else if (character != null ) {
       status_state = 2;
       console.log('actor is ' ,typeof character);
-      // if (character != 1 || character != 2 ){
-      //   responsetext = 'เลือกได้เฉพาะตัวที่ 1 หรือ 2 เท่านั้นนะคะ';
+      if (character != 1 || character != 2 ){
+        responsetext = 'เลือกได้เฉพาะตัวที่ 1 หรือ 2 เท่านั้นนะคะ';
 
-      // }
+      }
     }
     else if(play != null){
       console.log('play is ',play);
