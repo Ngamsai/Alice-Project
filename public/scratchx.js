b90c05094e8cb901e9f12b946135e8638c59c66e
Ngamsai <ngamsiri.bm@gmail.com>
2019-02-20 15:04:05 +0700

    add extension for state

--- public/scratchx.js
+++ public/scratchx.js
@@ -193,6 +193,9 @@ new (function() {
         else if (name_variable == 'delete'){
             deletecode = value_variable;
         }
+        else if (name_variable == 'state'){
+            state = value_variable;
+        }
     };
     
     ext.when_receive_data = function() {
